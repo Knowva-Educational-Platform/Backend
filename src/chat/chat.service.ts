92f7b383edcc5da5caaf22b27d9a058bb6f96602
AboEl3iz <k.hassan202077@gmail.com>
2025-09-06 22:06:46 +0300

    final change in chat

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -105,7 +105,7 @@ export class ChatService {
           select: {
             id: true,
             name: true,
-            capacity: true,
+            
             subject: {
               select: { id: true, title: true, description: true }
             },
